0c2beebbf78cf79e5dfe24edeb521a1275e5c44c
ALfredoAndrade14 <alfredo.andrade@ccc.ufcg.edu.br>
2021-03-30 11:48:36 -0300

    criptografia da senha e autenticação do usuario

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const dados = require('../model/cadastro');
+const bcrypt = require('bcrypt');
+
 
 router.get('/', (req, res) => {
     dados.find({}, (err, data) => {
@@ -27,5 +29,22 @@ router.post('/create', (req, res) => {
     });
 });
 
+router.post('/auth', (req, res) => {
+    const { email, password} = req.body;
+
+    if(!email || !password) return res.send({ erro: 'Dados insuficientes!'});
+
+    dados.findOne({email}, (err, data) => {
+        if(err) return res.send({error: 'Erro ao buscar usuario'})
+        if(!data) return res.send({error: 'Usuario não registrado'})
+
+        bcrypt.compare(password, data.password, (err, same) => {
+            if(!same) return res.send({erro: 'Erro ao autenticar'});
+            
+            data.password = undefined;
+            return res.send(data);
+        })
+    }).select('+password');
+});
 
 module.exports = router;
