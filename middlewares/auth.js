3cb1fc2ab9b8e6fff3ad094a5dcf989c50856ad5
Lucas Araújo de Lima <70279700+lucasarlim@users.noreply.github.com>
2021-04-15 09:06:49 -0300

    change auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,13 +5,13 @@ const auth = (req, res, next) => {
     const token_header = req.headers.auth;
     if(!token_header) return res.status(401).send({ error: 'Token não enviado!' });
 
-    jwt.verify(token_header, config.jwt_pass, (err, decoded) => {
+    jwt.verify(token_header, config.jwt_pass, (err, decoded, async function (next){
         if (err) return res.status(401).send({ error: 'Token inválido!' });
         const user = await Users.findOne({_id: decoded.sub });
         if (!user.token.includes(token_header)) return res.status(401).send({ error: 'Token inválido!' }); 
         req.id = decoded.sub
         return next();
-    });
+    }));
 }
 
 module.exports = auth;
